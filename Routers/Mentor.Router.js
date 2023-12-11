71686cbecccb1e86a3fa54571801a58298a7bf93
Safiya@0619 <saficute05@gmail.com>
2023-12-11 14:45:45 +0530

    Mentor Router error correction

--- Routers/Mentor.Router.js
+++ Routers/Mentor.Router.js
@@ -6,7 +6,7 @@ import {
   getMentorList,
   getStudentsForMentor,
   previousMentor,
-} from "../Controllers/Mentor.controller.js";
+} from "../Controllers/Mentor.Controller.js";
 
 const router = express.Router();
 
@@ -16,6 +16,5 @@ router.post("/assign", assignStudentToMentor); // to assign one student to one m
 router.post("/assignmultiple", MultiStudForOneMen); // to assign many students to one mentor
 router.post("/studentlist", getStudentsForMentor); // to get student list for one mentor
 router.post("/previousmentor", previousMentor); // to get student list for one mentor
-
 
 export default router;
